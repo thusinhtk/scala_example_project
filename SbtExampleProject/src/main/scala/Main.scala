9c775518f4f2e7a104c9ff4afe34b3029e95f873
Phuong Nguyen <minhphuong15hcb1@gmail.com>
2022-05-25 23:20:03 +0700

    Initial commit for example Scala project

---
+++ SbtExampleProject/src/main/scala/Main.scala
@@ -0,0 +1,22 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    /*  val */
+    /*
+    val x = 2
+    println(x)
+    */
+
+    /* Variable */
+    /*
+    var x: String = "Hi"
+    x = "Say hello instead"
+    println(x) // Say hello instead
+    */
+
+    /*  Block */
+    println({
+      val x = 10
+      x+1
+    })
+  }
+}
